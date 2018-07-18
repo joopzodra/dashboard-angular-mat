0453d0f895132b13eac057227112e19044759e7d
Joop Rosier <joop@zodra.nl>
2018-07-18 21:07:23 +0200

    removed unused breakpoints input property

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -10,22 +10,6 @@ import { Observable } from 'rxjs';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
 
-  cols = 1;
-  @Input() 'breakpoints': Observable<boolean[]>
-
-  constructor() { }
-
-  ngOnInit() {
-    this.breakpoints.subscribe(value => {
-      if (value[1]) {
-        this.cols = 3;
-      } else if (value[0]) {
-        this.cols = 2;
-      } else {
-        this.cols =1;
-      }
-    })
-  }
 }
