a85c9654be5fff5e60d79fb828f340c03cff36cd
Joop Rosier <joop@zodra.nl>
2018-07-19 16:59:50 +0200

    added mat-classes to template; in class added unsubscription on destroy.

--- src/app/dashboard/widgets/openweathermap-widget/openweathermap-widget.component.ts
+++ src/app/dashboard/widgets/openweathermap-widget/openweathermap-widget.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
-import { of, Observable } from 'rxjs';
+import { of, Observable, Subscription } from 'rxjs';
 import { MatSelect, MatSelectChange } from '@angular/material/select';
 import { environment } from '../../../../environments/environment'
 import { OpenweathermapService } from '../../../services/openweathermap/openweathermap.service';
@@ -16,30 +16,31 @@ import { currentWeather, forecast, iconDict, windSpeedBeaufort, windDirection, g
   templateUrl: './openweathermap-widget.component.html',
   styleUrls: ['./openweathermap-widget.component.scss']
 })
-export class OpenweathermapWidgetComponent implements OnInit {
+export class OpenweathermapWidgetComponent implements OnInit, OnDestroy {
 
   currentWeather = currentWeather;
   forecast: ForecastData[] = forecast;
   errorMessage = '';
   cityNames$: Observable<string[]> = of([]);
   @ViewChild('citySelect') citySelect!: MatSelect;
+  weatherSubscription!: Subscription;
 
   constructor(private service: OpenweathermapService) { }
 
   ngOnInit() {
-      let city = 'utrecht';
-      const cookies = document.cookie.split(';');
-      const cityKeyValue = cookies.filter((item) => item.includes('dashboardMdOpenweathermapCity='));
-      if(cityKeyValue.length) {
-        const cityValue = cityKeyValue[0].split('=')[1];
-        city = cityValue;
-      }
+    let city = 'utrecht';
+    const cookies = document.cookie.split(';');
+    const cityKeyValue = cookies.filter((item) => item.includes('dashboardMdOpenweathermapCity='));
+    if (cityKeyValue.length) {
+      const cityValue = cityKeyValue[0].split('=')[1];
+      city = cityValue;
+    }
     this.getWeatherData(city);
     this.cityNames$ = this.service.cityNames$;
   }
 
   getWeatherData(city: string) {
-    this.service.getWidgetWeather(city).subscribe(res => {
+    this.weatherSubscription = this.service.getWidgetWeather(city).subscribe(res => {
       this.handleWeatherData(res);
     },
       (err: HttpErrorResponse) => {
@@ -86,4 +87,7 @@ export class OpenweathermapWidgetComponent implements OnInit {
     return windDirection(degree);
   }
 
+  ngOnDestroy() {
+    this.weatherSubscription.unsubscribe();
+  }
 }
