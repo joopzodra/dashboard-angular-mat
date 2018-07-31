d43a63fff4a627e7b2d950bce1c459b916de2f7d
Joop Rosier <joop@zodra.nl>
2018-07-31 11:29:02 +0200

    Created IexPageComponent (specs unfinished yet).

--- src/app/pages/iex-page/iex-page.component.spec.ts
+++ src/app/pages/iex-page/iex-page.component.spec.ts
@@ -1,16 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BehaviorSubject, of } from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing'
 
 import { IexPageComponent } from './iex-page.component';
+import { ChartsModule } from '../../charts/charts.module';
+import { AppMaterialModule } from '../../app.material-module';
+import { DateIsoPipe } from '../../pipes/date-iso.pipe';
+import { IexService } from '../../services/iex/iex.service';
+import { IexDayItem, IexLongtermItem } from '../../models/iex-items';
 
 describe('IexPageComponent', () => {
   let component: IexPageComponent;
   let fixture: ComponentFixture<IexPageComponent>;
 
+  class stubIexService {
+    iexDayData$ = new BehaviorSubject<IexDayItem[]>([]);
+    iexLongtermData$ = new BehaviorSubject<IexLongtermItem[]>([]);
+
+    getDayData() {}
+    getLongTermData(companySymbol: string) {}
+
+  }
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ IexPageComponent ]
-    })
-    .compileComponents();
+      imports: [ChartsModule, AppMaterialModule, RouterTestingModule],
+      declarations: [IexPageComponent, DateIsoPipe],
+      providers: [{provide: IexService, useClass: stubIexService}]
+    });
   }));
 
   beforeEach(() => {
--- src/app/pages/iex-page/iex-page.component.ts
+++ src/app/pages/iex-page/iex-page.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, of, Subscription } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Location } from '@angular/common';
+
+import { BreakpointsService } from '../../services/breakpoints/breakpoints.service';
+import { IexDayItem, IexLongtermItem } from '../../models/iex-items';
+import { IexService } from '../../services/iex/iex.service';
+
+/*
+ * In the IexPageComponent, take care that the containing div gets a width of 100%. (If necessairy, continu adding a width of 100% to some parent elements.)
+ */
 
 @Component({
   selector: 'jr-iex-page',
@@ -6,10 +17,101 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./iex-page.component.scss']
 })
 export class IexPageComponent implements OnInit {
+  iexDayItems: IexDayItem[] = [];
+  iexSelectedCompanyDayItem!: IexDayItem;
+  iexSelectedCompanyLongtermItem!: IexLongtermItem;
+  chartHeight = 120;
+  weekdays = ['maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag', 'zondag'];
+  columns = 1;
+  breakpointsSubscription!: Subscription;
+  errorMessage = '';
+  selectedCompanyStyle = {};
+  pageContentStyle = {};
 
-  constructor() { }
+  constructor(private iexService: IexService, private breakpointsService: BreakpointsService, private location: Location) { }
 
   ngOnInit() {
+    this.breakpointsSubscription = this.breakpointsService.breakpoints$.subscribe(value => {
+      let screenSize;
+      if (value.large) {
+        screenSize = 'large';
+      } else if (value.medium) {
+        screenSize = 'medium';
+      } else if (value.tablet) {
+        screenSize = 'tablet'
+      } else {
+        screenSize = 'small';
+      }
+
+      if (screenSize === 'medium' || screenSize === 'large') {
+        this.columns = 2;
+        this.pageContentStyle = {
+          'flex-flow': 'row'
+        }
+        this.selectedCompanyStyle = {
+          'order': '2',
+          'max-width': 'none',
+          'margin-left': '16px',
+          'margin-top': '8px'
+        }
+      } else {
+        this.columns = 1;
+        this.pageContentStyle = {
+          'align-items': 'center'
+        };
+        this.selectedCompanyStyle = {};
+      }
+    });
+
+    this.iexService.iexDayData$.subscribe(data => {
+      if (data.length) {
+        this.iexDayItems = data;
+        // setInitialSelectedCompany needs iexDayItems, so call it after setting the iexDayItems.
+        this.setInitialSelectedCompany();
+      }
+    });
+    this.iexService.iexLongtermData$.subscribe(data => {
+      this.iexSelectedCompanyLongtermItem = <IexLongtermItem>data;
+    })
+    this.iexService.getDayData();
   }
 
+  ngOnDestroy() {
+    this.breakpointsSubscription.unsubscribe();
+  }
+
+  selectCompany(companySymbol: string) {
+    this.iexService.getLongtermData(companySymbol);
+    this.iexSelectedCompanyDayItem = this.iexDayItems.find(item => item.day.symbol === companySymbol) as IexDayItem;
+    document.cookie = "iexCompanySymbol=" + companySymbol + "; max-age=31536000"; // max-age is 60*60*24*365 seconds = 1 year*/
+    window.scrollTo({ top: 0, behavior: 'auto' });
+  }
+
+  setInitialSelectedCompany() {
+    // If the url which navigated to this component contains a query param, use this for the selected company. Then, replace the url by an url without query param.
+    // Else, if there's a cookie with an selected company, use this for the selected company.
+    // Else, use the default selected company.
+    const urlFragments = this.location.path().split('?');
+    const companySymbolQueryParam = urlFragments[1];
+    if (companySymbolQueryParam) {
+      const companySymbol = companySymbolQueryParam.split('=')[1];
+      this.selectCompany(companySymbol);
+      this.location.replaceState(urlFragments[0]);
+    } else if (companyInCookie()) {
+      this.selectCompany(<string>companyInCookie());
+    } else {
+      const defaultCompanySymbol = this.iexDayItems[0].day.symbol;
+      this.selectCompany(defaultCompanySymbol);
+    }
+
+    function companyInCookie() {
+      const cookies = document.cookie.split(';');
+      const companySymbol = cookies.filter((item) => item.includes('iexCompanySymbol='));
+      if (companySymbol.length) {
+        const symbol = companySymbol[0].split('=')[1];
+        return symbol;
+      }
+      return undefined;
+    }
+  }
 }
