d2c6349f3fdcc191c81ce864ceb57d9f136010cb
Joop Rosier <joop@zodra.nl>
2018-08-01 14:31:46 +0200

    IexPageComponent specs completed.

--- src/app/pages/iex-page/iex-page.component.spec.ts
+++ src/app/pages/iex-page/iex-page.component.spec.ts
@@ -1,42 +1,98 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BehaviorSubject, of } from 'rxjs';
 import { RouterTestingModule } from '@angular/router/testing'
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { HttpErrorResponse } from '@angular/common/http';
 
 import { IexPageComponent } from './iex-page.component';
-import { ChartsModule } from '../../charts/charts.module';
 import { AppMaterialModule } from '../../app.material-module';
 import { DateIsoPipe } from '../../pipes/date-iso.pipe';
 import { IexService } from '../../services/iex/iex.service';
 import { IexDayItem, IexLongtermItem } from '../../models/iex-items';
+import { stubIexDayItems, stubIexLongtermItem } from '../../testing/stub-iex-data';
 
 describe('IexPageComponent', () => {
   let component: IexPageComponent;
   let fixture: ComponentFixture<IexPageComponent>;
+  let el: HTMLElement;
+  let de: DebugElement;
+  let iexService: IexService;
 
   class stubIexService {
-    iexDayData$ = new BehaviorSubject<IexDayItem[]>([]);
-    iexLongtermData$ = new BehaviorSubject<IexLongtermItem[]>([]);
-
-    getDayData() {}
-    getLongTermData(companySymbol: string) {}
-
+    iexDayData$ = new BehaviorSubject<IexDayItem[]>(stubIexDayItems);
+    iexLongtermData$ = new BehaviorSubject<IexLongtermItem[]>([stubIexLongtermItem]);
+    getDayData() { }
+    getLongtermData(companySymbol: string) { }
   }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ChartsModule, AppMaterialModule, RouterTestingModule],
+      imports: [AppMaterialModule, RouterTestingModule],
       declarations: [IexPageComponent, DateIsoPipe],
-      providers: [{provide: IexService, useClass: stubIexService}]
+      providers: [{ provide: IexService, useClass: stubIexService }],
+      schemas: [NO_ERRORS_SCHEMA]
     });
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(IexPageComponent);
     component = fixture.componentInstance;
+    el = fixture.nativeElement;
+    de = fixture.debugElement;
+    iexService = TestBed.get(IexService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('displays Angular Material cards for the expected number of companies', async(() => {
+    fixture.whenStable().then(() => {
+      const companyCards = el.querySelectorAll('#all-companies-container mat-card');
+      expect(companyCards.length).toBe(2);
+      const cardTitle = companyCards[0].querySelector('.company-header');
+      expect((<HTMLElement>cardTitle).textContent).toBe('AAAA – Aaaa');
+    });
+  }));
+
+  it('has an selected company and passes the corresponding data to the IexChart components', async(() => {
+    fixture.whenStable().then(() => {
+      expect(component.iexSelectedCompanyDayItem).toBeTruthy();
+      expect(component.iexSelectedCompanyLongtermItem).toBeTruthy();
+    });
+  }));
+
+  it('on clicking on a company, this company becomes the selected company', async(() => {
+    const spy = spyOn((<any>component).iexService, 'getLongtermData');
+    fixture.whenStable().then(() => {
+      const secondCompanyCard = de.queryAll(By.css('#all-companies-container li'))[1];
+      secondCompanyCard.triggerEventHandler('click', null);
+      expect(component.iexSelectedCompanyDayItem).toBe(stubIexDayItems[1]);
+      expect(spy).toHaveBeenCalledWith(stubIexDayItems[1].day.symbol);
+    });
+  }));
+
+  it('displays an error message when IexService fails', async(() => {
+    const err = <HttpErrorResponse>{ error: 'an error' }
+    iexService.iexDayData$.next(err);
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      const errMessage = el.querySelector('.error-message');
+      expect((<HTMLElement>errMessage).textContent).toMatch('an error');
+      component.errorMessage = '';
+      return true;
+    })
+      .then(() => {
+        iexService.iexLongtermData$.next(err);
+        fixture.whenStable().then(() => {
+          fixture.detectChanges();
+          const errMessage = el.querySelector('.error-message');
+          expect((<HTMLElement>errMessage).textContent).toMatch('an error');
+        });
+      });
+  }));
+
 });
--- src/app/testing/stub-iex-data.ts
+++ src/app/testing/stub-iex-data.ts
@@ -8,13 +8,13 @@ const stubDayData = [
   { date: "20180725", time: "15:59", price: 218.17 },
 ];
 
-export const stubIexDayItem = {
-  company: 'AAAA',
+const stubIexDayItem1 = {
+  company: 'Aaaa',
   quote: {
     company_name: 'a company',
     latest_update: {
       amsterdamTimeIso: '2018-07-27T22:00:00.248+02:00',
-      newYorkTimeIso: '2018-07-27T16:00:00.248-04:0',
+      newYorkTimeIso: '2018-07-27T16:00:00.248-04:00',
       weekday: 5
     },
     latest_price: 217.5
@@ -24,3 +24,43 @@ export const stubIexDayItem = {
     chart_data: stubDayData
   }
 };
+
+const stubIexDayItem2 = {
+  company: 'Bbbb',
+  quote: {
+    company_name: 'a company',
+    latest_update: {
+      amsterdamTimeIso: '2018-07-27T22:00:00.248+02:00',
+      newYorkTimeIso: '2018-07-27T16:00:00.248-04:00',
+      weekday: 5
+    },
+    latest_price: 217.5
+  },
+  day: {
+    symbol: 'BBBB',
+    chart_data: stubDayData
+  }
+};
+
+export const stubIexDayItems = [stubIexDayItem1, stubIexDayItem2]
+
+const stubLongtermData = [
+  { date: "2018-07-25", price: 216 },
+  { date: "2018-06-25", price: 216.44 },
+  { date: "2018-05-25", price: 214.31 },
+  { date: "2018-04-25", price: 216.73 },
+  { date: "2018-03-25", price: 218.28 },
+  { date: "2018-02-25", price: 217.13 },
+  { date: "2018-01-25", price: 218.17 },
+];
+
+export const stubIexLongtermItem = {
+  month: {
+    symbol: 'AAAA',
+    chart_data: stubLongtermData
+  },
+  twoYears: {
+    symbol: 'AAAA',
+    chart_data: stubLongtermData
+  }
+}
