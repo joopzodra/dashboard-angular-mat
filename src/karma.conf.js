330efd7dfb1b881c377c87f9504225b90f16c5f2
Joop Rosier <joop@zodra.nl>
2018-08-03 14:51:03 +0200

    Changed browsers in karma config. (Three browser at the same time slow down the tests, causing a timeout conflict in one test.)

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -27,8 +27,8 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Safari', 'Firefox', 'Chrome'],
-    // browsers: ['Safari']
+    // browsers: ['Safari', 'Firefox'],
+    browsers: ['Chrome'],
     singleRun: false
   });
 };
