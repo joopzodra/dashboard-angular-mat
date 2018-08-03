f3f15dd01eb9a2d552ce5c2bfa35c94ee3564725
Joop Rosier <joop@zodra.nl>
2018-08-03 10:12:06 +0200

    Run tests in Chrome, Safari and Firefox.

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -27,7 +27,8 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Safari'],
+    browsers: ['Safari', 'Firefox', 'Chrome'],
+    // browsers: ['Safari']
     singleRun: false
   });
 };
